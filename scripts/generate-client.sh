e82a41b0064a805d483eb5dcb2e476261eb9a426
jpe0824 <jason.e24@gmail.com>
2025-01-06 08:31:19 -0700

    🎨 [pre-commit.ci] Auto format from pre-commit.com hooks

---
+++ scripts/generate-client.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+set -e
+set -x
+
+cd backend
+python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ./openapi.json
+cd ..
+cd frontend
+npm run generate-client
